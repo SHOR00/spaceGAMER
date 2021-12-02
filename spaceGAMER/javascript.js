8590aaa82fb19ffe9a2eee9e34879ef58a7f4590
SHOR00 <89482309+SHOR00@users.noreply.github.com>
2021-12-02 17:47:31 -0300

    Add files via upload

---
+++ spaceGAMER/javascript.js
@@ -0,0 +1,12 @@
+
+ScrollReveal().reveal('header');
+ScrollReveal().reveal('.inicio', { delay: 300 });
+ScrollReveal().reveal('.juegos', { delay: 300 });
+ScrollReveal().reveal('.youtubers', { delay: 300 });
+ScrollReveal().reveal('.mods', { delay: 300 });
+ScrollReveal().reveal('.video', { delay: 300 });
+ScrollReveal().reveal('.comunidad', { delay: 300 });
+ScrollReveal().reveal('.extra', { delay: 300 });
+ScrollReveal().reveal('.eventos', { delay: 300 });
+ScrollReveal().reveal('.contactos', { delay: 300 });
+ScrollReveal().reveal('.creditos', { delay: 300 });
